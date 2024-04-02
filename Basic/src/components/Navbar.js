4decd6d64f4aea412d8d08c425269f2e0e10e828
harishghasolia07 <100846446+harishghasolia07@users.noreply.github.com>
2024-04-02 20:35:42 +0530

    Add files via upload

---
+++ Basic/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Footer from './Footer'
+
+const Navbar = (props) => {
+  return (
+    <div>
+    <div className='logo'>{props.logoText}</div>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+        <Footer/>
+    </div>
+    
+  )
+}
+
+export default Navbar
